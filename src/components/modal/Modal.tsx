83e74c91e8595b82cd091cfa3752bec4ef6ed436
1445654576@qq.com <1445654576@qq.com>
2021-06-18 00:35:41 +0800

    fix: 更新代码

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,22 +1,59 @@
 import React from "react";
+import './style.scss';
 
 interface IState {
+  _show: boolean;
+}
+
+interface IProps {
+  title?: string;
+  footer?: any;
+  cancelText?: string;
+  confirmText?: string;
+  customMode?: boolean;
   show: boolean;
 }
 
-export default class Modal extends React.Component{
+export default class Modal extends React.Component<IProps, IState>{
   state: IState;
-  constructor(props: any) {
+  constructor(props: IProps) {
     super(props);
     this.state = {
-      show: false
+      _show: this.props.show
     }
   }
 
+  handleClick = () => {
+    this.setState({
+      _show: false
+    })
+    
+  }
+  
+  
+
   render() {
+    const {_show} = this.state;
+    const {children, title = '标题', cancelText = '取消', confirmText = '确定', customMode = false} = this.props;
     return (
-      <div className="modal">
-        {/* {...this.props.children} */}
+      <div className={`modal ${_show ? '' : 'hidden'}`} onClick={this.handleClick} >
+        {
+          customMode ? (
+            <div className="content">
+              {children}
+            </div>
+          ) : (
+            <div>
+              <div className="title">
+                {title}
+              </div>
+              <div className="content">
+                {children}
+              </div>
+            </div>
+          )
+        }
+
       </div>
     )
   }
--- src/components/operation-bar/OperationBar.tsx
+++ src/components/operation-bar/OperationBar.tsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.scss';
+import Modal from "../modal/Modal";
+
+
+interface IState {
+  showModal?: boolean;
+}
 
 // 操作栏
-export default function OperationBar() {
-  return (
-    <div className="operation-bar">
-      <div className="btn">添加</div>
-      <div className="btn">筛选</div>
-    </div>
-  )
+export default class OperationBar extends React.Component<IState> {
+  state: IState;
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      showModal: false
+    }
+  }
+  handleClick = () => {
+    this.setState({
+      showModal: true
+    })
+  }
+
+  // static getDerivedStateFromProps(pre: any, next: any) {
+  //   console.log('pre->', pre, 'next->', next);
+  //   return true;
+  // }
+  
+  render() {
+    const {showModal} = this.state;
+    return (
+      <div className="operation-bar">
+        <div className="btn" onClick={this.handleClick}>添加</div>
+        <div className="btn">筛选</div>
+        <Modal show={showModal as boolean}>
+          <div>222222</div>
+        </Modal>
+      </div>
+    )
+  }
 }
